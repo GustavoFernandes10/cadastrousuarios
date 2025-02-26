494475ef240b1c4e6ef8113907ff9d7c8177f0e2
GustavoFernandes10 <gustavo_fsantos@hotmail.com>
2025-02-25 21:44:36 -0300

    Upload do projeto
    Registro de nome – Programa para cadastro de usuário
    Versão – 1.0
    
    1.	Introdução
    Este documento tem o objetivo de descrever a especificação do sistema, um software desenvolvido para armazenar e gerenciar informações de usuários com base em seu CPF. O sistema permite o cadastro, consulta e exclusão de usuários de forma eficiente e segura, garantindo a integridade dos dados.
    1.1.	Finalidade
    O objetivo principal do sistema é proporcionar um meio simples e eficaz para registrar usuários utilizando Nome, Sobrenome, CPF e Cargo. A chave principal do cadastro é o CPF, garantindo que não tenha repetição do mesmo cadastro.
    O sistema oferece três funcionalidades principais:
    Cadastro de Usuário: Registra um novo usuário no sistema.
    Consulta de Usuário: Permite buscar um usuário pelo CPF. Se o CPF não estiver cadastrado, o sistema informa que o registro não foi encontrado.
    Exclusão de Usuário: Remove um usuário do sistema com base no CPF informado.
    1.2.	Escopo
    O sistema terá as seguintes características:
    Cadastro de Usuários com Nome, Sobrenome, CPF e Cargo.
    Chave única baseada no CPF, garantindo que cada usuário tenha um CPF exclusivo.
    Consulta de CPF para verificar se um usuário está registrado.
    Remoção de usuários pelo número do CPF.
    O sistema não abrange funcionalidades como recuperação de dados deletados, alteração de informações após o cadastro ou integração com bancos de dados externos.
    1.3.	Definições
    Usuário: Pessoa cadastrada no sistema com Nome, Sobrenome, CPF e Cargo.
    CPF: Cadastro de Pessoa Física – documento único utilizado como chave principal no sistema.
    Cadastro: Ação de inserir um novo usuário na base de dados.
    Consulta: Processo de busca de um usuário pelo CPF.
    Exclusão: Remoção de um usuário do sistema pelo CPF.
    1.4.	Visão Geral
    O Sistema será um software de fácil utilização. Ele utilizará arquivos para armazenar os dados e permitirá a manipulação eficiente dos registros.
    A usabilidade será priorizada, garantindo que qualquer pessoa consiga cadastrar, consultar e excluir usuários sem dificuldades. Mensagens claras serão exibidas para orientar o usuário em cada ação realizada.
    Com essa abordagem, o sistema proporcionará uma experiência intuitiva e prática para a gestão de cadastros de usuários.

---
+++ criacaoprogramaebac.cpp
@@ -0,0 +1,158 @@
+#include <stdio.h> //biblioteca de comunicação com o usuário
+#include <stdlib.h> //biblioteca de alocação de espaço em memória
+#include <locale.h> //biblioteca de alocação de texto por região - por ser Brasil por causa dos acentos
+#include <string.h> //biblioteca responsável por cuidar das string
+
+int registrar() //função responsável por cadastrar no sistema
+{
+	//inicio criação de variável/string
+	char arquivo[40];
+	char cpf[40];
+	char nome[40];
+	char sobrenome[40];
+	char cargo[40];
+	//fim criação de variável/string
+	
+	//coletar informação do usuário
+	printf("Digite o CPF a ser cadastrado: ");
+	scanf("%s", cpf); //%s refere-se a string e esta salvando
+	
+	strcpy(arquivo,cpf); //responsável por copiar os valores das string
+	
+	FILE *file; //cria o arquivo
+	file = fopen(arquivo, "w"); //w quer dizer escrever //cria o arquivo
+	fprintf(file,cpf); //salva o valor da variável
+	fclose(file); //fecha o arquivo
+	
+	file = fopen (arquivo, "a");
+	fprintf(file,",");
+	fclose(file);
+	
+	printf("Digite o nome a ser cadastrado: ");
+	scanf("%s", nome);
+	
+	file = fopen(arquivo, "a");
+	fprintf(file,nome);
+	fclose(file);
+	
+	file = fopen (arquivo, "a");
+	fprintf(file,",");
+	fclose(file);
+	
+	printf("Digite o sobrenome a ser cadastrado: ");
+	scanf("%s",sobrenome);
+	
+	file = fopen(arquivo, "a");
+	fprintf(file,sobrenome);
+	fclose(file);
+	
+	file = fopen (arquivo, "a");
+	fprintf(file,",");
+	fclose(file);
+	
+	printf("Digite o cargo a ser cadastrado: ");
+	scanf("%s",cargo);
+	
+	file = fopen(arquivo, "a");
+	fprintf(file,cargo);
+	fclose(file);
+	
+	system("pause");
+	
+}
+
+int consulta()
+{
+	setlocale(LC_ALL,"Portuguese"); //definindo a linguagem
+	
+	char cpf[40];
+	char conteudo[200];
+	
+	printf("Digite o CPF que deseja consultar: ");
+	scanf("%s", cpf);
+	
+	FILE *file;
+	file = fopen(cpf,"r");
+	
+	if(file == NULL)
+	{
+		printf("CPF não localizado!.\n");
+	}
+	
+	while(fgets(conteudo, 200 ,file) != NULL)
+	{
+		printf("\nEssas são as informações do usuário: ");
+		printf("%s", conteudo);
+		printf("\n\n");
+	}
+	
+	system("pause");
+	
+}
+
+
+int deleta()
+{
+	char cpf[40];
+	
+	printf("Digite o CPF a ser deletado: ");
+	scanf("%s",cpf);
+	
+	remove(cpf);
+		
+	FILE *file;
+	file = fopen(cpf,"r");
+
+	if (file == NULL)
+	{
+		printf("O usuário não se encontra no sistema! \n");
+		system("Pause");
+	}	
+}	
+
+
+
+int main ()
+	{
+		int opcao=0; //definindo variável de memória
+		int laco=1;
+		
+		for(laco=1;laco=1;)
+		{
+		
+			system("cls");
+		
+			setlocale(LC_ALL,"Portuguese"); //definindo a linguagem
+		
+			printf("______ Cartório da EBAC ______\n\n");
+			printf("Escolha a opção desejada do MENU:\n\n");
+			printf("\t 1 - Cadastrar nomes\n");
+			printf("\t 2 - Consultar nomes\n");
+			printf("\t 3 - Deletar nomes\n\n");
+			printf("Opção: "); //mensagem para digitar qual a opção
+		
+			scanf("%d", &opcao);  //scanf tem por objetivo scanea o que o usuário coloca e armazena o valor "%d" tem por objetivo armazenar a opção
+		
+			system("cls");  //tem por função limpar a tela, deletar as mensagens que tinha antes
+			
+			switch (opcao) //só aceita quando a opção for verdadeira caso seja falsa, usar default
+			{
+				case 1:
+				registrar();
+				break;
+				
+				case 2:
+				consulta();	
+				break;
+				
+				case 3:
+				deleta();
+				break;
+					
+				default:				
+				printf("Essa opção não está disponível!");
+				system("pause");
+				break;
+			}//fim da seleção								
+		}	
+	}	
